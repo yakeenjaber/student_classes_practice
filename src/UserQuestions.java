c65e41e8d10f3233781448ea1de067138ed7323b
akinkayagithub <akinkaya.b@gmail.com>
2022-04-28 09:39:56 -0500

    fix typo

--- src/UserQuestions.java
+++ src/UserQuestions.java
@@ -6,8 +6,8 @@ public class UserQuestions {
      HERE!
      Declare static final instance variables for interview questions below
      Would you like to be part of our Math or Science programs? (Y/N) -> variable name can be askToJoin
-     What is your firstName? -> variable name can be askFirstName
-     What is your lastName? -> variable name can be askLastName
+     What is your first name? -> variable name can be askFirstName
+     What is your last name? -> variable name can be askLastName
      How old are you? -> variable name can be askAge
      What is your gender? (F/M) -> variable name can be askGender
      What class would you like to join? (Math/Science) -> variable name can be askClassName
